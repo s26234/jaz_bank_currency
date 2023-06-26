effd0daf0388bcfc4ca9ab5fed881a1f3423af8e
s26234-pj <s26234@pjwstk.edu.pl>
2023-06-26 18:49:40 +0200

    initial

---
+++ src/main/java/com/example/jaz_s26234_nbp/JazS26234NbpApplication.java
@@ -0,0 +1,16 @@
+package com.example.jaz_s26234_nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS26234NbpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JazS26234NbpApplication.class, args);
+
+
+        System.out.println("hello");
+    }
+
+}
---
+++ src/test/java/com/example/jaz_s26234_nbp/JazS26234NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jaz_s26234_nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS26234NbpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
